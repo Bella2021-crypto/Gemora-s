adc825dd98f9c0a68998ef14c7908f0f0ce9e63b
Bella2021-crypto <82710380+Bella2021-crypto@users.noreply.github.com>
2025-08-14 12:41:50 +0100

    Add files via upload

--- _app.js
+++ _app.js
@@ -1,5 +1,4 @@
-import '@/styles/globals.scss';
-import { useEffect } from 'react';
+import '../styles/globals.scss';import Layout from '../components/Layout';import { useEffect } from 'react';
 export default function App({ Component, pageProps }){
   useEffect(()=>{ if('serviceWorker' in navigator){ navigator.serviceWorker.register('/sw.js').catch(()=>{});} },[]);
   return <Component {...pageProps} />;
